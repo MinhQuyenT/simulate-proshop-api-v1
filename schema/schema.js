4621db381d65818aa37e6d9dbd1d17fd46a201cd
MinhQuyen <tmquyenit@gmail.com>
2023-09-30 16:47:00 +0700

    add

--- schema/schema.js
+++ schema/schema.js
@@ -13,31 +13,31 @@ const typeDefs = gql`
         age: Int
     }
 
-    type Part {
-        partNumber: String
-        records {
-            activePlanner {
-                id: String
-                firstName: String
-                middleName: String
-                lastName: String
-                emailAddress: String
-                installGroup: String
-            },
-            clientPartRev: String
-            company {
-                companyName: String
-                contactEmail: String
-                contactEmail: String
-                billToAddress: String
-                billToCity: String
-                billToCountry: String
-                billToState: String
-                billToZipcode: String
-            },
-            customerPartNumber: String
-        }
-    }
+    # type Part {
+    #     partNumber: String
+    #     records {
+    #         activePlanner {
+    #             id: String
+    #             firstName: String
+    #             middleName: String
+    #             lastName: String
+    #             emailAddress: String
+    #             installGroup: String
+    #         },
+    #         clientPartRev: String
+    #         company {
+    #             companyName: String
+    #             contactEmail: String
+    #             contactEmail: String
+    #             billToAddress: String
+    #             billToCity: String
+    #             billToCountry: String
+    #             billToState: String
+    #             billToZipcode: String
+    #         },
+    #         customerPartNumber: String
+    #     }
+    # }
 
     #Root Type
     type Query {
